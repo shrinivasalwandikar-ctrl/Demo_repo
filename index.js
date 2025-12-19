d4202d385028b28911c141c3337a81396ec1b3bf
Shrinivas <shrinivasalwndikar@gmail.com>
2025-12-19 18:14:19 +0530

    port 3000

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(" Hi shrinvas welcome to Git Hub Actions");
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hi shrinvas welcome to Git Hub Actions\n');
+});
+
+server.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
