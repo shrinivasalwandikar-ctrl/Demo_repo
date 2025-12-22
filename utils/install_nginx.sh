1f3b2a276a31181b8c2671317dfa4741bb73a375
Shrinivas <shrinivasalwndikar@gmail.com>
2025-12-22 16:31:41 +0530

    Initial Python CI/CD setup

--- utils/install_nginx.sh
+++ utils/install_nginx.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
 DOMAIN_NAME=$1
 
 sudo apt update
 sudo apt install -y nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
 
-echo "Nginx installed for domain: $DOMAIN_NAME"
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
+server {
+    listen 80;
+    server_name $DOMAIN_NAME;
+
+    location / {
+        proxy_pass http://127.0.0.1:5000;
+    }
+}
+EOF
+
+sudo nginx -t && sudo systemctl reload nginx
+
